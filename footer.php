f18ed4c42d71b9dc4bdff291b162e4e2299ccb15
Iron318i <iron318i@gmail.com>
2021-03-31 16:16:18 +0300

    16-16

--- footer.php
+++ footer.php
@@ -12,11 +12,25 @@ defined('ABSPATH') || exit;
 <footer class="site-footer">
     <div class="container">
 	<div class="row">
-	    <div class="col-md-3 col-xl-auto footer-1">
+	    <div class="col-md-4 footer-1">
+		<p class="mb-lg-2"><a href="<?php echo site_url(); ?>" class="logo-link" rel="home"><img src="<?php echo get_template_directory_uri() ?>/img/main-logo.png" srcset="<?php echo get_template_directory_uri() ?>/img/main-logo-2x.png 2x" class="img-fluid" alt="<?php bloginfo('name'); ?>"></a></p>
+		<p>Cambridge Sleep Sciences combines the principles of science with cutting-edge technology to enhance brain activity during sleep and develop innovative products that improve quality of life and re-energises lives. </p>
+		<p>Cambridge Sleep Sciences Ltd is a subsidiary of the Barkby Group Plc.</p>
 	    </div>
-	    <div class="col-md-3 col-xl-auto footer-2">
+	    <div class="col-md-4 footer-2">
+		<?php
+		wp_nav_menu(
+			array(
+			    'theme_location' => 'footer',
+			    'container' => false,
+			    'container_class' => false,
+			    'menu_id' => 'footer-menu',
+			    'menu_class' => 'nav',
+			)
+		);
+		?>
 	    </div>
-	    <div class="col-md-3 col-xl-auto footer-3">
+	    <div class="col-md-4 footer-3">
 	    </div>
 	</div>
     </div>
